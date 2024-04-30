fdb2490937ebae0844ffc2b973863588326a35a7
YashaswiSV <vermayashaswi1@gmail.com>
2024-04-30 19:40:49 +0530

    done

---
+++ javascriptP(startingAni)/script.js
@@ -0,0 +1,12 @@
+//===============set time out======================
+
+function show(){
+    setTimeout(loader,3000);
+}
+
+const loader=()=>{
+    document.getElementById("webcode").style.display="block";
+    document.getElementById("img").style.display="none";
+
+
+}
